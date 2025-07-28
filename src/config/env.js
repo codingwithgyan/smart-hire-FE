b663ee61f2d2ddfdd4c2982bd927c6aeaae3141e
Gyaneshwar Kumar <codewithgyan@gmail.com>
2025-07-28 22:28:38 +0530

    BE URI

--- src/config/env.js
+++ src/config/env.js
@@ -1,14 +1,14 @@
 const config = {
-    local: {
-        api: {
-            baseUrl: "http://localhost:2345/api",
-        }
+  local: {
+    api: {
+      baseUrl: "http://localhost:2345/api",
     },
-    prod: {
-        api: {
-            baseUrl: "https://smart-hire-api.onrender.com/api",
-        }
-    }
+  },
+  prod: {
+    api: {
+      baseUrl: "https://smart-hire-be.vercel.app/api",
+    },
+  },
 };
 
 const env = process.env.REACT_APP_STAGE || "local";
